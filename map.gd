cf9026a40c71fbfd94672b65988931855b120702
DuckOfDisorder <105909412+DuckOfDisorder@users.noreply.github.com>
2025-04-02 19:33:33 +0000

    Fixed movement

--- map.gd
+++ map.gd
@@ -12,7 +12,8 @@ var screen_size # Size of the game window.
 var input_buffer = [0,0,0,0]
 var loaded_chunks = []
 var player_tile_pos: Vector2
-var moved = false
+var moved: bool
+var tosnap: Vector2
 var animation: AnimatedSprite2D
 
 # Called every time there's an input.
@@ -21,56 +22,68 @@ func _input(event):
 	bufferinit(event, "move_left", 1)
 	bufferinit(event, "move_down", 2)
 	bufferinit(event, "move_up", 3)
-
-func _ready():
-	# Set random seeds for noise variation
-	moisture.seed = randi()
-	temperature.seed = randi()
-	animation = get_node("Player/AnimatedSprite2D")
-	screen_size = get_viewport_rect().size
-
-func _process(delta):
+	
 	var velocity = Vector2.ZERO # The player's movement vector.
 	velocity.x += (input_buffer[0] - input_buffer[1])
 	velocity.y += input_buffer[2] - input_buffer[3]
 	velocity = map_to_local(velocity) - map_to_local(Vector2i(0,0))
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		animation.play()
-	else:
-		animation.stop()
-		
-	player.position += Vector2(velocity.x * delta, velocity.y * delta)
+		tosnap = velocity
+		print(tosnap)
 	
-		# Convert the player's position to tile coordinates
+func _ready():
+	moved = false
+	tosnap = Vector2(0,0)
+	# Set random seeds for noise variation
+	moisture.seed = randi()
+	temperature.seed = randi()
+	animation = get_node("Player/AnimatedSprite2D")
+	screen_size = get_viewport_rect().size
+
+func _process(_delta):
+	# Convert the player's position to tile coordinates
 	player_tile_pos = local_to_map(player.position)
-	generate_chunk(player_tile_pos)
+	generate_chunk(player_tile_pos, false)
 
-func _physics_process(delta):
-	if input_buffer.max()==0 && moved:
-		print(player_tile_pos)
-		print(player.position)
-		var tosnap = map_to_local(player_tile_pos) - player.position
-		player.position += tosnap / 5
-		if player.position == map_to_local(player_tile_pos):
-			moved = false
+func _physics_process(_delta):
+	if not moved:
+		tosnap = tosnap / 2
+		player.position += tosnap
 
-func generate_chunk(pos):
+func generate_chunk(pos, unload):
 	for x in range(width):
 		for y in range(height):
-			# Generate noise values for moisture, temperature, and altitude
-			var moist = moisture.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10 # Values between -10 and 10
+			var moist = moisture.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10
 			var temp = temperature.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10
-
-			set_cell(Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(round(3 * (moist + 10) / 20), round(3 * (temp + 10) / 20)))
+			var tile = Vector2(round(3 * (moist + 10) / 20), round(3 * (temp + 10) / 20))
+			
+			if unload:
+				set_cell(Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(-1,-1))
+			else:
+				set_cell(Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, tile)
 			
 			if Vector2i(pos.x, pos.y) not in loaded_chunks:
 				loaded_chunks.append(Vector2i(pos.x, pos.y))
+				unload_distant_chunks(pos)
+
+func unload_distant_chunks(player_pos):
+	var unload_distance_threshold = (width * 2) + 1
+
+	for chunk in loaded_chunks:
+		var displacement = Vector2(chunk) - player_pos
+		var distance = sqrt(displacement.dot(displacement))
+
+		if distance > unload_distance_threshold:
+			generate_chunk(chunk, true)
+			loaded_chunks.erase(chunk)
 
 func bufferinit(event, action, key):
 	if event.is_action_pressed(action):
 		input_buffer[key] += 1
+		animation.play()
 		moved = true
 	if event.is_action_released(action):
 		input_buffer[key] -= 1
+		animation.stop()
+		moved = false
