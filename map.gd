f535551f1598b3196d9f8a9bfbe66d9e4079ee68
scylecs <scylexniconeko@outlook.com>
2025-04-07 04:15:18 -0700

    tileless

--- map.gd
+++ map.gd
@@ -1,21 +1,25 @@
 extends TileMapLayer
 
+var loc_coord = [0,0]
+"""
 var moisture = FastNoiseLite.new()
 var temperature = FastNoiseLite.new()
 var width = 64
 var height = 64
 
+
 # Reference to the player character
-@export var player: Node2D
 @export var location_marker: TileMapLayer
-@export var inputs: Camera2D
 var screen_size # Size of the game window.
 var loaded_chunks = []
 var player_tile_pos: Vector2
 var tosnap: Vector2
-
+"""
+@export var player: Node2D
+@export var inputs: Camera2D
 func _ready():
 	player.moved = false
+	"""
 	tosnap = Vector2(0,0)
 	# Set random seeds for noise variation
 	moisture.seed = randi()
@@ -37,7 +41,7 @@ func _physics_process(_delta):
 		var tween = player.create_tween()
 		tween.tween_property(player, "position", player.position + tosnap, 0.15)
 		tosnap = Vector2.ZERO;
-
+"""
 # Called every time there's an input.
 func _input(event):
 	bufferinit(event, "move_right", 0)
@@ -46,28 +50,31 @@ func _input(event):
 	bufferinit(event, "move_up", 3)
 	bufferinit(event, "move_rup", 4)
 	bufferinit(event, "move_lup", 5)
-	
+	"""
 	var velocity = Vector2.ZERO # The player's movement vector.
 	velocity.x += (inputs.buffer[0] + inputs.buffer[4] - inputs.buffer[1] - inputs.buffer[5])
 	velocity.y += (inputs.buffer[2] - inputs.buffer[3] - inputs.buffer[4] - inputs.buffer[5])
 	velocity = map_to_local(velocity) - map_to_local(Vector2i(0,0))
 	
 	if velocity.length() > 0:
-		location_marker.highlighted = [local_to_map(player.position + velocity + Vector2(5,25))]
+		location_marker.highlighted = [local_to_map(player.position + velocity +11 Vector2(5,25))]
 		tosnap = velocity
-	
+	"""
 func bufferinit(event, action, key):
 	if event.is_action_pressed(action):
 		inputs.buffer[key] += 1
 		player.moved = true
+		loc_coord[0] += (inputs.buffer[0] + inputs.buffer[4] - inputs.buffer[1] - inputs.buffer[5])
+		loc_coord[1] += (inputs.buffer[1] + inputs.buffer[2] - inputs.buffer[3] - inputs.buffer[4])
+		print(loc_coord)
 		
 	if event.is_action_released(action):
-		player.position = map_to_local(player_tile_pos) + Vector2(5,-25)
-		print(player_tile_pos)
+#		player.position = map_to_local(player_tile_pos) + Vector2(5,-25)
+#		print(player_tile_pos)
 		inputs.buffer[key] -= 1
 		player.moved = false
-		location_marker.highlighted = []
-
+#		location_marker.highlighted = []
+"""
 func generate_chunk(pos, unload):
 	for x in range(width):
 		for y in range(height):
@@ -94,3 +101,4 @@ func unload_distant_chunks(player_pos):
 		if distance > unload_distance_threshold:
 			await generate_chunk(chunk, true)
 			loaded_chunks.erase(chunk)
+"""
