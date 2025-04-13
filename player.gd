b79792cfeee1ac3ecebfb6309dcd3efbb46a7c0c
DuckOfDisorder <105909412+DuckOfDisorder@users.noreply.github.com>
2025-04-13 12:45:43 +0000

    Made stuff pretty, saving works now (loading still doesn't)

--- player.gd
+++ player.gd
@@ -1,20 +1,12 @@
 extends Area2D
 
+const body_shapes = 3
+
 var moved: bool
 var played: bool = false
-var char_file = FileAccess.open("user://char.file", FileAccess.WRITE)
 @export var animation: AnimatedSprite2D
+var player_data
 
-func _ready() -> void:
-	while char_file.get_position() < char_file.get_length():
-		var json_string = char_file.get_line()
-		var json = JSON.new()
-		var parse_result = json.parse(json_string)
-		if not parse_result == OK:
-			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-			continue
-		var player_data = json.data
-		
 func _process(delta: float) -> void:
 	if moved and not played:
 		animation.play()
@@ -26,7 +18,7 @@ func _process(delta: float) -> void:
 			animation.play_backwards()
 			played = false
 
-func player():
+func player_save():
 	var player_props = {
 		"body_shape" : get_node("Body/AnimatedSprite2D").get_sprite_frames(),
 		"body_color" : get_node("Body/AnimatedSprite2D").get_animation(),
@@ -35,4 +27,27 @@ func player():
 		"hair_shape" : get_node("Hair/AnimatedSprite2D").get_sprite_frames(),
 		"hair_color" : get_node("Hair/AnimatedSprite2D").get_animation(),
 		}
+	print(player_props["body_shape"])
 	return player_props
+
+func player_load():
+	var char_file = FileAccess.open("user://char.file", FileAccess.READ)
+	var json_string = char_file.get_line()
+	var json = JSON.new()
+	var parse_result = json.parse(json_string)
+	if not parse_result == OK:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		return null
+	var player_data = json.data	
+	var n = 0
+	while n < body_shapes:
+		var body_shape = load("res://player" + str(n) + ".tres")
+		var body_name = body_shape.to_string()
+		print(body_name)
+		print(player_data["body_shape"])
+		if body_name == player_data["body_shape"]:
+			get_node("Body/AnimatedSprite2D").set_sprite_frames(body_shape)
+			break
+		n += 1
+	get_node("Body/AnimatedSprite2D").set_animation(player_data["body_color"])
+	return"loaded"
--- player_creation.gd
+++ player_creation.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 @export var ui_container: CanvasLayer
 @export var player: Node2D
+
 var screen_size = Vector2.ZERO
 var bg: ColorRect
 var prev: Button
 var next: Button
+var save: Button
 var body: AnimatedSprite2D
 
 var n = 0
-var changed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,23 +20,35 @@ func _ready() -> void:
 	bg = ui_container.get_node("ColorRect")
 	prev = ui_container.get_node("Prev")
 	next = ui_container.get_node("Next")
+	save = ui_container.get_node("Save")
 	body = player.get_node("Body/AnimatedSprite2D")
 	
 	bg.size = screen_size
 	player.position = Vector2(screen_size.x/1.2, screen_size.y/2)
 	prev.position = player.position - Vector2(25, -80)
 	next.position = prev.position + Vector2(25,0)
-	player.get_node("Body/AnimatedSprite2D").scale = Vector2(1,1)
+	save.position = next.position + Vector2(-25,40)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if prev.button_pressed and not changed:
-		print("hi")
-		var body_shape_1 : SpriteFrames = load("res://player" + str(n) + ".tres")
+	prev.pressed.connect(_button_handler.bind(-1))
+	next.pressed.connect(_button_handler.bind(1))
+	save.pressed.connect(_save_handler)
+	
+	player.get_node("Body/AnimatedSprite2D").scale = Vector2(1,1)
+	print(player.player_load())
+
+func _button_handler(add: int):
+	print("hi")
+	n = n + add
+	if n < player.body_shapes and n >= 0:
+		var body_shape_1: SpriteFrames = load("res://player" + str(n) + ".tres")
 		body.set_sprite_frames(body_shape_1)
 		print(body.get_sprite_frames())
 		print(body_shape_1)
+	elif n >= player.body_shapes:
+		n = player.body_shapes-1
+	else: n = 0
 
-		changed = true
-	elif changed and not prev.button_pressed:
-		changed = false
+func _save_handler():
+	var char_file = FileAccess.open("user://char.file", FileAccess.WRITE)
+	char_file.store_line(JSON.stringify(player.player_save()))
+	print("Saved!")
